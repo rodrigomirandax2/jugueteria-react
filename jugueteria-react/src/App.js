cb126f4f55b9220bc35933ff1c3c38daf4f57895
rodrigo.miranda <rodrigo.miranda@west-ingenieria.cl>
2024-01-10 08:51:04 -0300

    fix css cartWdiget

--- jugueteria-react/src/App.js
+++ jugueteria-react/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer/Footer.jsx';
 import NavBar from './components/NavBar/NavBar.jsx';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx';
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <NavBar/>
       <ItemListContainer msg='Aqui irá el contenido de mi componente'/>
+      <Footer className='Footer'/>
     </div>
   );
 }
---
+++ jugueteria-react/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css';
+function Footer() {
+  return (
+    <div className='div'>
+        <label className='label-footer'>Derechos Reservados ©</label>
+    </div>
+  )
+}
+
+export default Footer
