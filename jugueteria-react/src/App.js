980e50e0b5ef132ece8ff5dd8c1dd0af5c2b5302
rodrigo.miranda <rodrigo.miranda@west-ingenieria.cl>
2024-01-29 17:09:37 -0300

    itemListContainer v1

--- jugueteria-react/src/App.js
+++ jugueteria-react/src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Footer from './components/Footer/Footer.jsx';
 import NavBar from './components/NavBar/NavBar.jsx';
+import { Routes, Route} from 'react-router-dom';
+import Inicio from './components/Inicio/Inicio.jsx';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx';
-
+import Empresa from './components/Empresa/Empresa.jsx';
 function App() {
   return (
     <div>
       <NavBar/>
-      <ItemListContainer msg='Aqui irá el contenido de mi componente'/>
-      <Footer className='Footer'/>
+      <Routes>
+        <Route path="/" element={ <Inicio /> } />
+        <Route path="figuras" element={ <ItemListContainer /> } />
+        <Route path="empresa" element={ <Empresa /> } />
+      </Routes>
     </div>
   );
 }
--- jugueteria-react/src/components/ItemListContainer/ItemListContainer.jsx
+++ jugueteria-react/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { CircularProgress, Box, Grid, Card, CardMedia, CardActionArea, CardContent, Typography } from '@mui/material';
+import data from '../../api.json';
 import './ItemListContainer.css'
 
-function ItemListContainer(props) {
+function ItemListContainer() {
+  const [figures, setFigures] = useState(null);
+
+  useEffect(() => {
+    const api = setTimeout(() => {
+      setFigures(data);
+    }, 2000);
+    return () => clearTimeout(api);
+  }, []);
+
   return (
-    <p className='title'>{props.msg}</p>
-  )
+   <>      
+    {figures === null ? (
+      <>
+        <Box sx={{display: 'flex',justifyContent: 'center',alignItems: 'center',height: '100vh'}}>
+          <CircularProgress />
+        </Box>
+      </>) : (
+        <Grid container spacing={4}>
+          {
+            figures.map((f) => (
+              <Grid item xs={3}>
+                <CardActionArea>
+                  <Card className='Card'>
+                    <CardMedia 
+                      component="img"
+                      image={f.image}
+                      alt={f.nombre}
+                      style={{
+                        height: '50vh',
+                        maxWidth: '100%',
+                        maxHeight: '60vh'
+                      }}
+                    />
+                    <CardContent>
+                      <Typography className='Typography'>{f.nombre}</Typography>
+                      {f.descuento ? (
+                        <>
+                          <div style={{ display: 'inline-block' }}>
+                            <Typography className='Descuento' style={{ display: 'inline' }}>${f.valor}  </Typography>
+                            <Typography className='Oferta' style={{ display: 'inline' }}>{f.porcentaje_descuento}% OFF </Typography>
+                            <Typography className='Precio' style={{ display: 'inline' }}>${f.valor - (f.valor*(f.porcentaje_descuento/100))}</Typography>
+                          </div>
+                        </>
+                      ) : (
+                        <Typography className='Precio'>${f.valor}</Typography>
+                      )}
+                    </CardContent>
+                  </Card>
+                </CardActionArea>
+              </Grid>
+            ))
+          }
+        </Grid>
+      )}
+    </>
+  );
 }
 
-export default ItemListContainer
+export default ItemListContainer;
